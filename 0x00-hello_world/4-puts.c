af774d18ff099171a466c7160f68c2e317bc227e
Abelastro <platoonabel@gmail.com>
2021-08-26 13:11:06 -0700

    put.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 
-  puts("\"Progamming is like building a multilingual puzzle");
-  return (0);
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
 
 }
