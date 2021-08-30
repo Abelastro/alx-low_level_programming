9c9dd3b1bb11962eb9e3e120f3bb32c4f9d3c570
Abelastro <platoonabel@gmail.com>
2021-08-30 13:13:10 -0700

    hey

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,12 +4,13 @@
  * followed by a new line, using putchar
  * Return: Always 0 (Success)
  */
+int main(void)
 {
-  int n;
-  for (n = 48; n < 58; n++)
-    {
-      putchar(n);
-    }
-  putchar('\n');
-  return (0);
+int n;
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
 }
